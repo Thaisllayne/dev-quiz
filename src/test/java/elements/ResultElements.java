c6c830527d2b0e15a0c0f06958f31ec7f07dd853
Thaisllayne Santana <thaisllaynesantana@gmail.com>
2023-08-06 17:05:15 -0300

    add testes referentes a page Result

--- src/test/java/elements/ResultElements.java
+++ src/test/java/elements/ResultElements.java
@@ -1,4 +1,13 @@
 package elements;
 
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
 public class ResultElements {
+
+    public ElementsCollection table = $$("#root > div > main > table > tbody tr");
+    public SelenideElement totalHits = $("#root > div > main > table > tfoot > tr > td:nth-child(4)");
 }
--- src/test/java/pages/ResultTest.java
+++ src/test/java/pages/ResultTest.java
@@ -1,4 +1,55 @@
 package pages;
 
-public class ResultTest {
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import elements.ResultElements;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utils.HomeUtils;
+import utils.QuizUtils;
+
+public class ResultTest extends HomeUtils {
+
+    static ResultElements resultElements = new ResultElements();
+    static HomeUtils homeUtils = new HomeUtils();
+    static QuizUtils quizUtils = new QuizUtils();
+
+    @Test
+    public void compararRespostaEnviadaComGabarito(){
+        homeUtils.iniciarQuiz("Thai", "Easy");
+        quizUtils.finalizarQuiz();
+
+        ElementsCollection table = resultElements.table;
+        for (SelenideElement row : table){
+            SelenideElement correctAnswer = row.$("td:nth-child(2)");
+            SelenideElement selectedAnswer = row.$("td:nth-child(3)");
+            SelenideElement resultIcon = row.$("td:nth-child(4) > span");
+
+            if (correctAnswer.getText().equals(selectedAnswer.getText())){
+                Assert.assertEquals(correctAnswer.getText(), selectedAnswer.getText());
+                Assert.assertEquals(resultIcon.getText(), "✔");
+            } else {
+                Assert.assertNotEquals(correctAnswer.getText(), selectedAnswer.getText());
+                Assert.assertEquals(resultIcon.getText(), "✖");
+            }
+        }
+    }
+
+    @Test
+    public void validarTotalDeAcertos(){
+        homeUtils.iniciarQuiz("Thai", "Easy");
+        quizUtils.finalizarQuiz();
+
+        int totalAcertos = 0;
+
+        ElementsCollection table = resultElements.table;
+        for (SelenideElement row : table){
+            SelenideElement resultIcon = row.$("td:nth-child(4) > span");
+            if (resultIcon.getText().equals("✔")){
+                totalAcertos++;
+            }
+        }
+        int totalHitsInt = Integer.parseInt(resultElements.totalHits.getText());
+        Assert.assertEquals(totalAcertos, totalHitsInt);
+    }
 }
